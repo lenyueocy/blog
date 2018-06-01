471141305fbc650767d31d6c4946bbeae8230d54
lenyue <493708759@qq.com>
2018-06-01 12:02:58 +0800

    ./初始化仓库

---
+++ runtime/temp/ff29a3d4156d96f1d9aecf854f7589a5.php
@@ -0,0 +1,17 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:80:"/Users/lenyue/E HD/www/tp5bk30/public/../application/admin/view/login/index.html";i:1527008006;}*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>管理登陆 - <?php echo \think\Config::get('auth.blogname'); ?></title>
+    <link rel="stylesheet" href="admin_css/style.css"/>
+    <script src="home_js/jquery-1.10.1.min.js"></script>
+    <script src="com_layer/layer.js"></script>
+</head>
+<body class="login-bg" data-url="<?php echo url('Base/login'); ?>" data-tip="<?php echo isset($tip) ? $tip : 0; ?>">
+    <div class="login-nav"></div>
+    <script>
+        alert(1);
+    </script>
+</body>
+</html>
