ed3dfbe59fe372db9fb54283e3f529caea0134b6
lenyue <493708759@qq.com>
2018-07-01 22:53:27 +0800

    增加

---
+++ public/pull.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * author by OCY, 2018/07/01 13:12.
+ */
+echo `cd /srv/www/blog && git pull`;
+$msg = `git pull`;
+echo '<br />';
+//echo `git pull`;
+echo '<br />';
+echo '。。。。。。。。。。。。。。。。。。。。。。。。<br />';
+echo 'pull成功';
