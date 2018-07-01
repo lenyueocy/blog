a5b1b4ff76774750789799aac1ea191251767107
lenyue <493708759@qq.com>
2018-07-01 23:07:05 +0800

    oktest

--- public/pull.php
+++ public/pull.php
@@ -2,7 +2,7 @@
 /**
  * author by OCY, 2018/07/01 13:12.
  */
-echo system('cd /srv/www/blog && git pull');
+echo system('cd /srv/www/blog && git pull >> /tmp/test.log');
 echo `cd /srv/www/blog && git pull`;
 $msg = `git pull`;
 echo '<br />';
