f58e7787ec6849123c862a18c43bfdd3fd79c080
lenyue <493708759@qq.com>
2018-07-02 10:19:44 +0800

    test

--- public/pull.php
+++ public/pull.php
@@ -4,7 +4,7 @@
  */
 $data = $_REQUEST;
 $payload = json_decode(file_get_contents('php://input'), true);
-file_put_contents('test_input.txt',var_export($payload).PHP_EOL,FILE_APPEND);
+file_put_contents('test_input.txt',var_export($payload,TRUE).PHP_EOL,FILE_APPEND);
 file_put_contents('test.txt','自动pull成功------->form to '.time().PHP_EOL,FILE_APPEND);
 echo system('cd /srv/www/blog && git pull 2>&1');
 echo `cd /srv/www/blog && git pull`;
