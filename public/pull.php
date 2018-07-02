c7dab5d77e6d6fb7bcf5113854b9cdc279bafacc
lenyue <493708759@qq.com>
2018-07-02 09:49:28 +0800

    test

--- public/pull.php
+++ public/pull.php
@@ -2,6 +2,8 @@
 /**
  * author by OCY, 2018/07/01 13:12.
  */
+$data = $_REQUEST;
+file_put_contents('test.txt','测试成功');
 echo system('cd /srv/www/blog && git pull 2>&1');
 echo `cd /srv/www/blog && git pull`;
 $msg = `git pull`;
