154ba397be4d27618148d41b1aebb066a7adefc7
lenyue <493708759@qq.com>
2018-07-02 10:16:54 +0800

    test

--- public/pull.php
+++ public/pull.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * author by OCY, 2018/07/01 13:12.111
+ * author by OCY, 2018/07/01 13:12.1
  */
 $data = $_REQUEST;
-$payload = file_get_contents('php://input');
-file_put_contents('test_input.txt',$payload.PHP_EOL,FILE_APPEND);
+$payload = json_decode(file_get_contents('php://input'), true);
+file_put_contents('test_input.txt',var_export($payload).PHP_EOL,FILE_APPEND);
 file_put_contents('test.txt','自动pull成功------->form to '.time().PHP_EOL,FILE_APPEND);
 echo system('cd /srv/www/blog && git pull 2>&1');
 echo `cd /srv/www/blog && git pull`;
