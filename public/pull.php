cdd62157f364a3d0acc3148d7a0cf3a5b2cdc2e3
lenyue <493708759@qq.com>
2018-07-02 10:15:22 +0800

    test

--- public/pull.php
+++ public/pull.php
@@ -3,6 +3,8 @@
  * author by OCY, 2018/07/01 13:12.1
  */
 $data = $_REQUEST;
+$payload = file_get_contents('php://input');
+file_put_contents('test_input.txt',$payload.PHP_EOL,FILE_APPEND);
 file_put_contents('test.txt','自动pull成功------->form to '.time().PHP_EOL,FILE_APPEND);
 echo system('cd /srv/www/blog && git pull 2>&1');
 echo `cd /srv/www/blog && git pull`;
