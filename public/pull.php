c4692bd911e7917ec9b2032534b62373d8b28c09
lenyue <493708759@qq.com>
2018-07-02 10:38:45 +0800

    test优化自动pull

--- public/pull.php
+++ public/pull.php
@@ -2,15 +2,19 @@
 /**
  * author by OCY, 2018/07/01 13:12.1
  */
-$data = $_REQUEST;
+
+
 $payload = json_decode(file_get_contents('php://input'), true);
-file_put_contents('test_input.txt',var_export($payload,TRUE).PHP_EOL,FILE_APPEND);
-file_put_contents('test.txt','自动pull成功------->form to '.time().PHP_EOL,FILE_APPEND);
-echo system('cd /srv/www/blog && git pull 2>&1');
-echo `cd /srv/www/blog && git pull`;
-$msg = `git pull`;
-echo '<br />';
-//echo `git pull`;
-echo '<br />';
-echo '。。。。。。。。。。。。。。。。。。。。。。。。<br />';
-echo 'pull成功';
+if(!$payload){
+    `cd /srv/www/blog && git pull origin master 2>&1`;
+    echo '<br />';
+    echo '<br />';
+    echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<br />';
+    echo '触发pull成功';
+}else{
+    header("Content-type: text/html; charset=utf-8");
+    file_put_contents('test_input.txt',var_export($payload,TRUE).PHP_EOL,FILE_APPEND);
+    file_put_contents('test.txt','自动pull成功------->form to '.date('Y-m-d H:i',time()).PHP_EOL,FILE_APPEND);
+    `cd /srv/www/blog && git pull origin master 2>&1`;
+}
+die("done " . date('Y-m-d H:i:s', time()));
