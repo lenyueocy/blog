eafa858758ee38404afbf385bbf492cc516665d4
lenyue <493708759@qq.com>
2018-07-02 09:59:06 +0800

    ~

--- public/pull.php
+++ public/pull.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * author by OCY, 2018/07/01 13:12.11
+ * author by OCY, 2018/07/01 13:12.
  */
 $data = $_REQUEST;
-file_put_contents('test.txt',var_export($_REQUEST,TRUE));
+file_put_contents('test.txt','自动pull成功------->form to '.tiem().PHP_EOL,FILE_APPEND);
 echo system('cd /srv/www/blog && git pull 2>&1');
 echo `cd /srv/www/blog && git pull`;
 $msg = `git pull`;
